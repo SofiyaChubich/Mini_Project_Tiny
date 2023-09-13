ff94a7bf3a0dc288f03fdc1e34630e92a470e3e6
Sofiya Chubich <95042138+SofiyaChubich@users.noreply.github.com>
2023-09-13 02:16:09 +0000

    uhhh this should work in theory

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def boss_fight(boss):
         if ch_roll >= boss["AC"]:
             boss["HP"] -= roll_dice(ch["Weapon"].split("d")[0], ch["Weapon"].split("d")[1])
 
-        boss_roll = roll_dice(1, 20)
+        boss_roll = roll_dice(1, 20) + boss["modifier"]
         chance = roll_dice(1, 2)
         if boss_roll >= ch["AC"]:
             ch["HP"] -= roll_dice(boss["Weapon"].split("d")[0], boss["Weapon"].split("d")[1])
 
         if chance == 1:
-            special_skill()
+            special_skill(boss["legendary_sk"])
 
 
 
@@ -45,6 +45,7 @@ def side_event():
 def get_stats(string):
     data = load_data("backgr.json")[string]
     data["equipment"] = []
+    data["status"] = []
     return data
 
 def update_stats():
@@ -56,8 +57,16 @@ def roll_dice(count, num):
         roll += random.randint(1, num)
     return roll
 
-def special_skill():
-    pass
+def special_skill(skill):
+    global ch
+    dict_ = load_data("special_skills.json")[skill]
+    if skill["damage"] != 0:
+        ch["HP"] -= roll_dice(skill["damage"].split("d")[0], skill["damage"].split("d")[1])
+    if skill["status"] != "None":
+        ch["status"].append(skill["status"])
+
+
+
 
 
 cl = random.choice(["barbarian", "bard", "rogue"])
@@ -67,6 +76,4 @@ for i in range(1, 4):
     main_event(main_q[str(i)])
     if i != 3:
         for _ in range(3):
-            side_event()
-    
- 
+            side_event() 
