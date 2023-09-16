5a46f87bfa7f86495b3d06c4559a3ca035aa1749
Sofiya Chubich <95042138+SofiyaChubich@users.noreply.github.com>
2023-09-16 00:16:01 +0000

    pls help

--- main.py
+++ main.py
@@ -10,40 +10,96 @@ def load_data(string):
 
 def boss_fight(boss):
     global ch
-
     while boss["HP"] > 0 and ch["HP"] > 0:
-        ch_roll = roll_dice(1, 20)
-        if cl == "barbarian":
-            ch_roll += ch["Strength"]
-        else:
-            ch_roll += ch["Dexterity"]
+        choice = ""
+        while choice != "1" and choice != "2" and choice != "3":
+            choice = input("1. Attack\n2. Guard\n3.Special skill\n(Please type in the number of your choice)\n")
+        if choice == "1":
+            print(ch["Attack_s"])
+            if "charming" in ch["status"] or "hidden" in ch["status"]:
+                ch_roll = max(roll_dice(1, 20), roll_dice(1, 20))
+            else:
+                ch_roll = roll_dice(1, 20)
+            if cl == "barbarian":
+                ch_roll += ch["Strength"]
+            else:
+                ch_roll += ch["Dexterity"]
 
-        if ch_roll >= boss["AC"]:
-            boss["HP"] -= roll_dice(ch["Weapon"].split("d")[0], ch["Weapon"].split("d")[1])
+            if ch_roll >= boss["AC"]:
+                damage = roll_dice(int(ch["Weapon"].split("d")[0]), int(ch["Weapon"].split("d")[1]))
+                print("You deal ", damage, " points of damage.")
+                boss["HP"] -= damage
+                if "raging" in ch["status"]:
+                    boss["HP"] -= 2
+                if "hidden" in ch["status"]:
+                    boss["HP"] -= roll_dice(1, 6)
+            else:
+                print("You deal no damage.")
+            if "hidden" in ch["status"]:
+                ch["status"].remove("hidden")
+        elif choice == "3":
+            special_skill(ch["Skill"])
+        if choice != "2" and "hidden" not in ch["status"]:
+            print(boss["Attack_s"])
+            if "charming" in ch["status"]:
+                boss_roll = min(roll_dice(1, 20), roll_dice(1, 20)) + boss["Modifier"]
+            else:
+                boss_roll = roll_dice(1, 20) + boss["Modifier"]
+            chance = roll_dice(1, 2)
+            if boss_roll >= ch["AC"]:
+                damage = roll_dice(int(boss["Weapon"].split("d")[0]), int(boss["Weapon"].split("d")[1]))
+                ch["HP"] -= damage
+                print("You suffer ", damage, " points of damage.")
+            else:
+                print("It misses")
 
-        boss_roll = roll_dice(1, 20) + boss["modifier"]
-        chance = roll_dice(1, 2)
-        if boss_roll >= ch["AC"]:
-            ch["HP"] -= roll_dice(boss["Weapon"].split("d")[0], boss["Weapon"].split("d")[1])
-
-        if chance == 1:
-            special_skill(boss["legendary_sk"])
-
-
+            if chance == 1:
+                print("Your opponent used ", boss["Legendary_sk"])
+                special_skill(boss["Legendary_sk"])
+        if choice == "2" and "raging" in ch["status"]:
+            ch["status"].remove("raging")
+        if choice == "2":
+            print("You heal two points of damage.")
+            ch["HP"] += 2
+        
 
 def main_event(event):
+    print(event["Intro"])
     boss_fight(event)
     if ch["HP"] <= 0:
-        pass
+        print(event["Outro L"])
     else:
-        pass
+        print(event["Outro W"])
 
 
-def side_event():
-    pass
+def side_event(number_ev):
+    global ch
+    event = load_data("events.json")[number_ev]
+    choice = ""
+    while choice != "1" and choice != "2" and choice != "3":
+        choice = input("1. Persuasion check\n2. Strength check\n3. Dexterity check\n(Please type in the number of your choice)")
+    success = True
+    if choice == "1":
+        roll = roll_dice(1, 20) + ch["Persuasion"]
+        if roll < event["P_AC"]:
+            success = False
+    elif choice == "2":
+        roll = roll_dice(1, 20) + ch["Strength"]
+        if roll < event["S_AC"]:
+            success = False
+    elif choice == "3":
+        roll = roll_dice(1, 20) + ch["Dexterity"]
+        if roll < event["D_AC"]:
+            success = False
 
-def get_stats(string):
-    data = load_data("backgr.json")[string]
+    if success:
+        if choice == "1":
+            
+    else:
+        boss_fight(event["boss"])
+
+def get_stats(cl):
+    data = load_data("backgr.json")[cl]
     data["equipment"] = []
     data["status"] = []
     return data
@@ -58,25 +114,32 @@ def roll_dice(count, num):
     return roll
 
 def special_skill(skill):
-    if skill["dependant"]:
-    chance = roll_dice(1, 2)
-    if chance == 1:
     global ch
-    dict_ = load_data("special_skills.json")[skill]
-    if skill["damage"] != 0:
-        ch["HP"] -= roll_dice(skill["damage"].split("d")[0], skill["damage"].split("d")[1])
-    if skill["status"] != "None":
-        ch["status"].append(skill["status"])
+    skill = load_data("special_skills.json")[skill]
+    chance = random.randint(1, 2)
+    if not skill["dependant"] or chance == 2:
+        if skill["damage"] != 0:
+            damage = roll_dice(int(skill["damage"].split("d")[0]), int(skill["damage"].split("d")[1]))
+            ch["HP"] -= damage
+            print("You suffer ", damage, " points of damage.")
+        if skill["status"] != "None":
+            ch["status"].append(skill["status"])
+            print("You are ", skill["status"])
 
 
 
 
 
 cl = random.choice(["barbarian", "bard", "rogue"])
-ch = get_stats("backgr.json")[cl]
+ch = get_stats(cl)
 main_q = load_data("main_scen.json")
+print("You wake up in what looks to be an abandoned wherehouse.\nYou don't remember who you are or where you came from.")
+print(ch["Intro"])
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for i in range(1, 4):
     main_event(main_q[str(i)])
     if i != 3:
         for _ in range(3):
-            side_event() 
+            ran = random.choice(list_)
+            list_.remove(ran)
+            side_event(ran) 
